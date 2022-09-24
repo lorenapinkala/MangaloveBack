0472155ed5215b1e2f3a28169ab337f9aeab0bf1
LorenaPinkala <lorenapinkala77@gmail.com>
2022-09-24 11:09:11 -0300

    first commit

---
+++ api/db/index.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+const db= new Sequelize('manga', null, null,{
+    host: 'localhost',
+   dialect: 'postgres',
+    logging: false
+})
+
+module.exports= db
---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+//const models=require('./models')
+
+const db = require("./db"); //carpeta de configuracion de db
+//const router = require("./routes");
+require("dotenv").config();
+
+// logging middleware
+app.use(morgan("tiny"));
+
+// parsing middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+//app.use('/api', router)
+
+// error middleware -> https://expressjs.com/es/guide/error-handling.html
+app.use((err, req, res, next) => {
+  console.log("ERROR");
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
+db.sync({ force: false }).then(() => {
+  app.listen(3001, () => console.log("server levantado en puerto 3001"));
+});
